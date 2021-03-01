708a9f77e06c0d57e13141512324a255a0561acb
Kaikai Liu <kaikai.liu@sjsu.edu>
2021-02-28 23:04:38 -0800

    Finished simple recyclerview

--- app/src/main/java/sjsu/cmpelkk/myappandroid/DisplayMessageActivity.kt
+++ app/src/main/java/sjsu/cmpelkk/myappandroid/DisplayMessageActivity.kt
@@ -2,7 +2,10 @@ package sjsu.cmpelkk.myappandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,33 @@ class DisplayMessageActivity : AppCompatActivity() {
         val messagetextView = findViewById<TextView>(R.id.messagetextView).apply {
             text = message
         }
+
+        //for recyclerview
+        val adapter = MyRecyclerAdapter()
+        val recyclerView: RecyclerView = findViewById(R.id.myrecyclerview)
+        recyclerView.adapter = adapter
     }
+}
+
+class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+
+class MyRecyclerAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    val data = List(30) {index ->  index*2}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
+
+        return TextItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.textView.text = item.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
 }
--- app/src/main/java/sjsu/cmpelkk/myappandroid/PostActivity.kt
+++ app/src/main/java/sjsu/cmpelkk/myappandroid/PostActivity.kt
@@ -6,14 +6,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 
 const val EXTRA_MESSAGE = "sjsu.cmpelkk.MyAppAndroid.MESSAGE"
+const val IMAGE_REQUEST_CODE = 33
 
 class PostActivity : AppCompatActivity() {
+    lateinit var myImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        //val myImage: ImageView = findViewById(R.id.uploadimageView)
+        myImage = findViewById(R.id.uploadimageView)
+        myImage.setOnClickListener{changeImage()}
+
+    }
+
+    private fun changeImage() {
+        //val myImage: ImageView = findViewById(R.id.uploadimageView)
+        //myImage.setImageResource(R.drawable.ic_baseline_star_rate_24)
+
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent,IMAGE_REQUEST_CODE)
     }
 
     fun sendMessage(view: View) {
@@ -29,9 +46,13 @@ class PostActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK)
+        if (resultCode == Activity.RESULT_OK && requestCode != IMAGE_REQUEST_CODE)
         {
             Toast.makeText(this,"Activity result received", Toast.LENGTH_LONG).show()
+        }
+
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_REQUEST_CODE) {
+            myImage.setImageURI(data?.data)
         }
     }
 }
